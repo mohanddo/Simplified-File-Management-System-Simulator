0a6b207c022e415a89095d26704bc8e7dccd17a9
mohanddo <mohandmeghaoui0@gmail.com>
2024-12-05 10:55:13 +0100

    Limited the taille of each bloc, and the numbre of blocs

--- bloc.c
+++ bloc.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-#include <bloc.h>
+#include "bloc.h"
+
+#define MAX_NOMBRE_BLOC 1000
+#define MAX_TAILLE_BLOC 1000
 
 int initDisk(int* nombreBloc, int* tailleBloc) {
     printf("Entrer le nombre de blocs: ");
@@ -8,9 +11,21 @@ int initDisk(int* nombreBloc, int* tailleBloc) {
         return -1;
     }
 
+    if (*nombreBloc > MAX_NOMBRE_BLOC)
+    {
+        printf("Max bloc number is: %d, please enter a smaller number \n", MAX_NOMBRE_BLOC);
+        return -1;
+    }
+    
     printf("Entrer la taille de bloc: ");
     if(scanf("%d", tailleBloc) != 1) {
         printf("Error, please enter a valid integer \n");
+        return -1;
+    }
+
+    if (*tailleBloc > MAX_TAILLE_BLOC)
+    {
+        printf("Max taille bloc is: %d, please enter a smaller number \n", MAX_TAILLE_BLOC);
         return -1;
     }
 
