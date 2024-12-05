fde97a0e89f57d7bc8a2447e424f19679919fc10
mohanddo <mohandmeghaoui0@gmail.com>
2024-12-05 10:00:40 +0100

    Created the fichier.h header and the MetaDonnees struct

---
+++ fichier.h
@@ -0,0 +1,9 @@
+typedef MetaDonnees MetaDonnees;
+struct MetaDonnees
+{
+    char nomFichier[30];
+    int tailleFichierBlocs, tailleFichierEngistrements;
+    int addressPremierBloc;
+    int modeOrganisationGlobal, modeOrganisationInterne;
+
+};
